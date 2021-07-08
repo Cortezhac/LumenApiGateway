1cda618a60b55e375f6d92ba20dd66832167048c
Irvin Cortez <irvincotez67@gmail.com>
2021-07-08 16:06:16 -0600

    add controllers author and book

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+
+    public function index(){
+
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function show($id){
+
+    }
+
+    public function update(Request $request, $id){
+
+    }
+
+    public function destroy($id){
+
+    }
+}
