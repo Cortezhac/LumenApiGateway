d7250dc38a163832f474100cec6e8009e1c7e3d6
Irvin Cortez <irvincotez67@gmail.com>
2021-07-08 20:26:41 -0600

    install GuzzleHttp

---
+++ app/Traits/ConsumesExternalService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Traits;
+use GuzzleHttp\Client;
+
+Trait ConsumesExternalService {
+    /**
+     * Send to request to any service
+     * @param $method
+     * @param $requestUri
+     * @param array $formParams
+     * @param array $headers
+     * @return string
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function perfomRequest($method, $requestUri, $formParams = [] , $headers = []){
+        $client = new Client([
+           'base_uri' => $this->baseUri
+        ]);
+
+        $response = $client->request($method, $requestUri, [
+            'form_params' =>$formParams,
+            'headers' => $headers
+        ]);
+
+        return $response->getBody()->getContents();
+    }
+}
